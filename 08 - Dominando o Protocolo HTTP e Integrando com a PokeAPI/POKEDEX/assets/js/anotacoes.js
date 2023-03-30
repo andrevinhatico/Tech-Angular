a6d37ebc6f4ed3be62e3fc30366f4492026284a7
Andre Ribeiro <andrebahia7@gmail.com>
2023-03-30 16:24:06 -0300

    api html

--- 08 - Dominando o Protocolo HTTP e Integrando com a PokeAPI/POKEDEX/assets/js/anotacoes.js
+++ 08 - Dominando o Protocolo HTTP e Integrando com a PokeAPI/POKEDEX/assets/js/anotacoes.js
@@ -16,3 +16,39 @@ fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => console.log(jsonBody))
     .catch((error) => console.log(error))
+
+
+
+//     main -----------------------------
+    
+// const pokemonList = document.getElementById('pokemonList')
+
+
+// pokeApi.getPokemons().then((pokemons = []) => {
+     
+//  // pokemons.map((value, index, array)) => {
+//  //     return ``
+//  // }
+
+//     const newList = pokemons.map((pokemon) => {
+//      return convertPokemonToLi(pokemon)
+//  })
+ 
+//  //console.log(newList);
+
+//  const newHtml = newList.join('')
+
+//  pokemonList.innerHTML += newHtml
+ 
+//  // const lisItens = []  
+
+
+//  //     for (let i = 0; i < pokemons.length; i++) {
+//  //         const pokemon = pokemons[i];
+//  //         lisItens.push(convertPokemonToLi(pokemon))
+//  //     }
+//  })
+//  .catch((error) => console.log(error))
+
+
+//     main----------------
